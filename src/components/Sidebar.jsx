b52404b7f7f7622cfe769ef3556781a075c6b10b
AbreuGeorgie <abreu.georgie@gmail.com>
2023-07-12 16:49:34 +0200

    bouton navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,41 @@
 import { NavLink } from "react-router-dom";
-import avatar from "../assets/avatar.png"
+import avatar from "../assets/avatar.png";
+import "./Sidebar.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function Sidebar() {
   return (
-    <div>
-      <div>
-        <img src={avatar} alt="avatar"></img>
-      </div>
-      <ul className="navigation">
-        <li>
-          <NavLink to="/" >
-            Accueil
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" >
-            A propos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/skills" >
-            Compétences
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="projects" >
-            Réalisations
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="contact" >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Image src={avatar} roundedCircle alt="avatar"/>
+        </Col>
+      </Row>
+      <Row className="navigation">
+        <ul>
+          <li>
+            <NavLink to="/" className="btn-nav">
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="btn-nav">A propos</NavLink>
+          </li>
+          <li>
+            <NavLink to="/skills" className="btn-nav">Compétences</NavLink>
+          </li>
+          <li>
+            <NavLink to="projects" className="btn-nav">Réalisations</NavLink>
+          </li>
+          <li>
+            <NavLink to="contact" className="btn-nav">Contact</NavLink>
+          </li>
+        </ul>
+      </Row>
+    </Container>
   );
 }
 export default Sidebar;
