56819291fb39fd2b1ad28a8c0df053480905c6aa
AbreuGeorgie <abreu.georgie@gmail.com>
2023-07-13 14:30:45 +0200

    mise en place du burgermenu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,45 @@
-import { NavLink } from "react-router-dom";
-import avatar from "../assets/avatar.png";
-import "./Sidebar.scss";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import avatar from "../assets/avatar.png";
+import { SidebarButton } from "./SidebarButton";
 
 function Sidebar() {
   return (
     <Container>
-      <Row>
-        <Col>
-          <Image src={avatar} roundedCircle alt="avatar"/>
+      <Row className="navigation justify-content-center">
+        <Col lg={12} className="text-center">
+          <Image
+            src={avatar}
+            roundedCircle
+            alt="avatar"
+            width={200}
+            height={200}
+          />
         </Col>
       </Row>
-      <Row className="navigation">
-        <ul>
-          <li>
-            <NavLink to="/" className="btn-nav">Accueil</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" className="btn-nav">A propos</NavLink>
-          </li>
-          <li>
-            <NavLink to="/skills" className="btn-nav">Compétences</NavLink>
-          </li>
-          <li>
-            <NavLink to="projects" className="btn-nav">Réalisations</NavLink>
-          </li>
-          <li>
-            <NavLink to="contact" className="btn-nav">Contact</NavLink>
-          </li>
-        </ul>
+
+      <Row>
+        <Col lg={12}>
+          <Navbar expand="lg" className="flex-column">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="flex-column">
+                <SidebarButton buttonText="" text="Accueil" />
+                <SidebarButton buttonText="about" text="A propos de moi" />
+                <SidebarButton buttonText="skills" text="Mes compétences" />
+                <SidebarButton buttonText="projects" text="Mes projets réalisés" />
+                <SidebarButton buttonText="contact" text="Contactez-moi !" />
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
       </Row>
     </Container>
   );
 }
+
 export default Sidebar;
