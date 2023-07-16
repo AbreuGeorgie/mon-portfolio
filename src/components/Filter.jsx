eeba2dcfdc499dde339ea1b167bf282d3248f211
AbreuGeorgie <abreu.georgie@gmail.com>
2023-07-16 03:01:24 +0200

    filtres fonctionnels

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,19 @@
+import React, { useState } from 'react';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 
-function Filter() {
+function Filter({change}) {
+
   return (
 
-    <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-    <ToggleButton id="tbg-radio-1" value={1} variant='secondary' className='text-white'>
+    <ToggleButtonGroup type="radio" name="options" defaultValue="Frontend" onChange={change}>
+    <ToggleButton id="tbg-radio-1" value="Frontend" variant='secondary' className='text-white'>
       Frontend
     </ToggleButton>
-    <ToggleButton id="tbg-radio-2" value={2} variant='secondary' className='text-white'>
+    <ToggleButton id="tbg-radio-2" value="Backend" variant='secondary' className='text-white'>
       Backend
     </ToggleButton>
-    <ToggleButton id="tbg-radio-3" value={3} variant='secondary' className='text-white'>
+    <ToggleButton id="tbg-radio-3" value="Projet" variant='secondary' className='text-white'>
       Gestion de projet
     </ToggleButton>
   </ToggleButtonGroup>
