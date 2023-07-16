6e9e31f37e203296d18f2ca97524707aead69a7d
AbreuGeorgie <abreu.georgie@gmail.com>
2023-07-16 03:05:01 +0200

    filtres fonctionnels

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,38 @@
-
+import React, { useState } from 'react';
 import Filter from "../components/Filter";
 import MyCard from "../components/MyCard";
+import datas from "../datas.json";
+import { Link } from "react-router-dom";
+import './Projects.scss'
 
 function Projects() {
+  const [filterCategory, setFilter] = useState("Frontend");
+  const handleChange = (e) =>{
+    setFilter(e)
+  }
+  console.log(filterCategory)
+
   return (
     <div>
-      <header>
+      <header className="mb-5">
         <h1>Mes projets réalisés</h1>
       </header>
-      <main>
-        <div className="filter-bar">
-          <Filter />
-        </div>
-        <div className="projects-container">
-        <MyCard
-        cover="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-        image="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
-        title="HTML"
-        technologies="HTML/CSS"/>
-        </div>
+      <main> 
+        <section className="filter-bar">
+          <Filter change={handleChange}/>
+        </section>
+
+        <section className="cardsContainer bg-none my-2">
+          {datas.filter(card => card.category === filterCategory).map((card) => (
+            <Link to={card.site} className="projectCard" key={card.id}>
+              <MyCard
+                cover={card.cover}
+                title={card.title}
+                technologies={card.technologies}
+              />
+            </Link>
+          ))}
+        </section>
       </main>
     </div>
   );
