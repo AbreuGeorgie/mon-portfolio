6906b9eae0846ad180eeb05d29aa13c89d9ec6f5
AbreuGeorgie <abreu.georgie@gmail.com>
2023-07-24 18:44:13 +0200

    création de la modale

---
+++ src/components/Modale.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+} from 'mdb-react-ui-kit';
+
+export default function MyModal({title, description, technologies}) {
+  const [basicModal, setBasicModal] = useState(false);
+
+  const toggleShow = () => setBasicModal(!basicModal);
+
+  return (
+    <>
+      <MDBBtn onClick={toggleShow}>LAUNCH DEMO MODAL</MDBBtn> 
+      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex='-1'>
+        <MDBModalDialog>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>{title}</MDBModalTitle>
+              <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody>
+                <p>
+                    Description du projet : {description}
+                </p>
+                <p>
+                    Technologies utilisées : {technologies}
+                </p>
+            </MDBModalBody>
+
+            <MDBModalFooter>
+              <MDBBtn color='secondary' onClick={toggleShow}>
+                Close
+              </MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+    </>
+  );
+}
